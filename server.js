e396ba34feed33f60e02bd9f2a6636f0c570d538
Alik-5 <aaleqsanyan048@gmail.com>
2026-03-24 12:42:45 +0400

    123

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const PORT = 3000;
 
 app.post("/api/register", (req, res) => {
     res.json(req.body);
 });
 
 app.get("/api/register", (req, res) => {
-    res.json({ email: req.body.email, password: req.body.password });
+    res.json({
+        
+         email: req.body.email, 
+         password: req.body.password 
+        
+});
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
